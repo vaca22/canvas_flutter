99442364b453cda1eace72024aff538ee8490f84
vaca22 <wsmslgh@gmail.com>
2023-07-08 00:06:06 +0800

    sdfsdf

---
+++ lib/duoek_file.dart
@@ -0,0 +1,119 @@
+import 'dart:typed_data';
+
+import 'package:flutter/foundation.dart';
+
+
+class DuoEkFile {
+  var filterWaveDataLength = 0;
+
+  final int COM_MAX_VAL = 127;
+  final int COM_MIN_VAL = -127;
+  final int COM_EXTEND_MAX_VAL = 382;
+  final int COM_EXTEND_MIN_VAL = -382;
+
+  final int COM_RET_ORIGINAL = -128;
+  final int COM_RET_POSITIVE = 127;
+  final int COM_RET_NEGATIVE = -127;
+
+  final int UNCOM_RET_INVALI = -32768;
+
+  final Uint8List originalData;
+   List<int> fileData=[];
+   List<int> unCompressData=[];
+   List<int> waveData=[];
+   List<double> waveDataDouble=[];
+
+  DuoEkFile({required this.originalData});
+
+  int unCompressNum = 0;
+  int lastCompressData = 0;
+  var length = 0;
+
+  void init() {
+    filterWaveDataLength = 0;
+    unCompressNum = 0;
+    lastCompressData = 0;
+    fileData = originalData.map((e) => e.toSigned(8)).toList();
+    length=fileData.length-30;
+
+  }
+
+  int unCompressAlgECG(int compressData) {
+    int ecgData = 0;
+    switch (unCompressNum) {
+      case 0:
+        if (compressData == COM_RET_ORIGINAL) {
+          unCompressNum = 1;
+          ecgData = UNCOM_RET_INVALI;
+        } else if (compressData == COM_RET_POSITIVE) {
+          unCompressNum = 3;
+          ecgData = UNCOM_RET_INVALI;
+        } else if (compressData == COM_RET_NEGATIVE) {
+          unCompressNum = 4;
+          ecgData = UNCOM_RET_INVALI;
+        } else {
+          ecgData = lastCompressData + compressData;
+          lastCompressData = ecgData;
+        }
+        break;
+      case 1:
+        lastCompressData = compressData & 0xFF;
+        unCompressNum = 2;
+        ecgData = UNCOM_RET_INVALI;
+        break;
+      case 2:
+        ecgData = lastCompressData + (compressData << 8);
+        lastCompressData = ecgData;
+        unCompressNum = 0;
+        break;
+      case 3:
+        ecgData = COM_MAX_VAL + (lastCompressData + (compressData & 0xFF));
+        lastCompressData = ecgData;
+        unCompressNum = 0;
+        break;
+      case 4:
+        ecgData = COM_MIN_VAL + (lastCompressData - (compressData & 0xFF));
+        lastCompressData = ecgData;
+        unCompressNum = 0;
+        break;
+      default:
+        break;
+    }
+    return ecgData;
+  }
+
+  //    public static double short2mv(short s) {
+  //         return (s * (1.0035 * 1800) / (4096 * 178.74));
+  //     }
+
+  double short2mv(int s) {
+    return (s * (1.0035 * 1800) / (4096 * 178.74));
+  }
+
+  void uncompress() {
+    init();
+    var endNullValueCount = 0;
+    for(int i=0;i<length-30;i++){
+      int tmp=unCompressAlgECG(fileData[10+i]);
+      if(tmp!=-32768&&filterWaveDataLength<length){
+        unCompressData.add(tmp);
+        filterWaveDataLength++;
+      }
+    }
+
+    for(int j=filterWaveDataLength-1;j>=0;j--){
+      if(unCompressData[j]==32767){
+        endNullValueCount++;
+      }else{
+        break;
+      }
+    }
+    filterWaveDataLength-=endNullValueCount;
+    waveData=unCompressData.sublist(0,filterWaveDataLength);
+    for(int i=0;i<filterWaveDataLength;i++){
+      waveDataDouble.add(short2mv(waveData[i]));
+    }
+  }
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 
+import 'duoek_file.dart';
+
 final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 late Uint8List fileData;
 
@@ -30,11 +32,16 @@ class MyApp extends StatelessWidget {
         body: Container(
           color: Colors.white,
           padding: EdgeInsets.symmetric(horizontal: 40, vertical: 80),
-          child: Container(
-            width: double.infinity,
-            height: double.infinity,
-            color: Colors.white,
-            child: CustomPaint(painter: FaceOutlinePainter()),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 3,
+                  width: MediaQuery.of(context).size.width,
+                  child: CustomPaint(painter: FaceOutlinePainter()),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -50,14 +57,44 @@ class FaceOutlinePainter extends CustomPainter {
     // Define a paint object
     final paint = Paint()
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 2.0
+      ..strokeWidth = 1.0
       ..color = Colors.black;
 
     print("fileData.length: ${fileData.length}");
-    final mouth = Path();
-    mouth.moveTo(size.width * 0.8, size.height * 0.6);
-    mouth.lineTo(200, 300);
-    canvas.drawPath(mouth, paint);
+    DuoEkFile duoEkFile = DuoEkFile(originalData: fileData);
+    duoEkFile.uncompress();
+    var lineSize = 831;
+    var hhh=3;
+    var co=74.283167;
+    var pointSize = duoEkFile.waveDataDouble.length;
+    var totalHighNumber =0;
+    var totalHigh=0.0;
+    if(pointSize%lineSize==0) {
+      totalHighNumber = pointSize ~/ lineSize;
+      totalHigh = pointSize / lineSize * hhh * co;
+    }else{
+      totalHighNumber = pointSize ~/ lineSize+1;
+      totalHigh = (pointSize / lineSize+1) * hhh * co;
+    }
+
+
+    var nv=size.width/lineSize;
+    for(int k=0;k<totalHighNumber;k++){
+      for(int j=0;j<lineSize;j++){
+        var index = k*lineSize+j;
+        if(index<duoEkFile.waveDataDouble.length-1){
+          var baseH = k*co*hhh+co*hhh/2.0;
+          var a = baseH-duoEkFile.waveDataDouble[index]*co;
+          var a2 = baseH-duoEkFile.waveDataDouble[index+1]*co;
+          canvas.drawLine(Offset(j.toDouble()*nv,a), Offset((j+1).toDouble()*nv,a2), paint);
+        }
+      }
+    }
+
+    // final mouth = Path();
+    // mouth.moveTo(size.width * 0.8, size.height * 0.6);
+    // mouth.lineTo(200, 300);
+    // canvas.drawPath(mouth, paint);
   }
 
   @override
